7c65bf2e9eb294e4753efcd4cb7847113cac9702
Dimitri Kleyn <dimitri.kleyn@fv.nrw.de>
2020-10-09 18:56:03 +0200

    add getbyid and create methods.

--- backend/src/main/java/de/deminator/zeiterfassung/TimeRestController.java
+++ backend/src/main/java/de/deminator/zeiterfassung/TimeRestController.java
@@ -1,12 +1,21 @@
 package de.deminator.zeiterfassung;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
+@RequestMapping("/api")
 public class TimeRestController {
 
     private final TimeEntryRepository repository;
@@ -15,10 +24,28 @@ public class TimeRestController {
         this.repository = repository;
     }
 
-    @CrossOrigin
-    @GetMapping("/api/times")
-    public List<TimeEntryDay> getTimes() {
-        return repository.findAll();
+    @GetMapping("/times")
+    public ResponseEntity<List<TimeEntryDay>> getTimes() {
+        List<TimeEntryDay> all = repository.findAll();
+        if (all.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/times/{id}")
+    public ResponseEntity<TimeEntryDay> getTimesById(@PathVariable("id") String id) {
+        Optional<TimeEntryDay> item = repository.findById(id);
+
+        return item.map(timeEntryDay -> new ResponseEntity<>(timeEntryDay, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/times")
+    public ResponseEntity<TimeEntryDay> createTimes(@RequestBody TimeEntryDay item) {
+        TimeEntryDay newItem = repository.save(item);
+        return new ResponseEntity<>(newItem, HttpStatus.CREATED);
     }
 
 }
