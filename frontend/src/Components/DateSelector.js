6e59b0964f103b196f6865d19feafe948a27d5b1
Rekhyt2901 <alex@deminator.de>
2020-10-15 12:37:47 +0200

    frontend update

--- frontend/src/Components/DateSelector.js
+++ frontend/src/Components/DateSelector.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function DateSelector() {
+function DateSelector(props) {
+    const [showMenu, setShowMenu] = useState(false);
+    const [startDate, setStartDate] = useState(new Date());
+
+    let monthArray = ["Januar", "Februar", "März", "April", "Mai", "Juni",
+        "Juli", "August", "September", "Oktober", "November", "Dezember"];
+    const [monthIndex, setMonthIndex] = useState(9);
+
+    let daysArray = [];
+    for (let i = 1; i <= 31; i++) {
+        daysArray.push(<div key={i}>{i}</div>);
+    }
+
+    const [year, setYear] = useState(startDate.getFullYear());
+
+    let dateSelectorPopup = <div className="DateSelectorPopup">
+        <div className="DateSelectorMonths">
+            <button onClick={() => {
+                setMonthIndex(monthIndex - 1);
+                if (monthIndex < 1) setMonthIndex(11);
+            }}>&#9664;</button>
+            <span>{monthArray[monthIndex]}</span>
+            <button onClick={() => {
+                setMonthIndex(monthIndex + 1);
+                if (monthIndex > 10) setMonthIndex(0);
+            }}>&#9654;</button>
+
+        </div>
+        <div className="DateSelectorDays">{daysArray}</div>
+        <div className="DateSelectorYears">
+            <button onClick={() => setYear(year - 1)}>&#9664;</button>
+            <span>{year}</span>
+            <button onClick={() => setYear(year + 1)}>&#9654;</button>
+        </div>
+    </div>
+    let dateSelectorMask = <div className="DateSelectorMask">
+    </div>
+
     return (
-        <div>
-
+        <div className="DateSelector">
+            <span onClick={() => setShowMenu(true)}>Date Selector</span><span onClick={() => setShowMenu(true)}>{props.date}</span>
+            <div>{showMenu ? dateSelectorPopup : null}</div>
+            <div onClick={() => setShowMenu(false)}>{showMenu ? dateSelectorMask : null}</div>
         </div>
     );
 }
--- frontend/src/Components/StartStopButton.js
+++ frontend/src/Components/StartStopButton.js
@@ -2,9 +2,26 @@ import React from 'react';
 
 function StartStopButton() {
     return (
-        <div>
-            <button className="StartStopButton">Start</button>
-        </div>
+        <button onClick={
+            async () => {
+                let newEntry = {
+                    "user": "jesus",
+                    "date": "2020-15-10",
+                    "times": [
+                        { "start": "08:08", "stop": "08:09" },
+                        { "start": "9:09", "stop": "9:10" }
+                    ]
+                }
+                let data = await fetch("http://localhost:8080/api/times", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: newEntry
+                });
+                console.log(data);
+            }
+        } type="button" className="StartStopButton">Start</button>
     );
 }
 
