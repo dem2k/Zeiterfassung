b38e841230e326f9f5bbbd0b0c542a2b278160b2
Dimitri Kleyn <dimitri.kleyn@fv.nrw.de>
2020-10-21 14:23:27 +0200

    Add simple DELETE-Method.

--- backend/src/main/java/de/deminator/zeiterfassung/TimeRestController.java
+++ backend/src/main/java/de/deminator/zeiterfassung/TimeRestController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,6 +47,15 @@ public class TimeRestController {
     public ResponseEntity<TimeEntry> createTimes(@RequestBody TimeEntry item) {
         TimeEntry newItem = repository.save(item);
         return new ResponseEntity<>(newItem, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/times/{id}")
+    public ResponseEntity<TimeEntry> deleteById(@PathVariable("id") String id) {
+        Optional<TimeEntry> item = repository.findById(id);
+        return item.map(it -> {
+            repository.deleteById(it.getId());
+            return new ResponseEntity<>(it, HttpStatus.OK);
+        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
 }
